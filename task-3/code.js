16663c96da3bf08676f1ebd813d6cc18d3b8fd41
bellobrissi297 <vovavasciaev@gmail.com>
2025-07-11 12:51:12 +0300

    My tasks

---
+++ task-3/code.js
@@ -0,0 +1,9 @@
+function digitalRoot(n) {
+  while(n >= 10){
+    n = n.toString()
+      .split('')
+      .map(Number)
+      .reduce((a, b) => a + b, 0)
+  }
+  return n;
+}
---
+++ task-4/code.js
@@ -0,0 +1,11 @@
+var maxSequence = function(arr){
+  let maxSum = 0;
+  let currentSum = 0;
+
+  for (let num of arr) {
+    currentSum = Math.max(0, currentSum + num);
+    maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return maxSum;
+}
